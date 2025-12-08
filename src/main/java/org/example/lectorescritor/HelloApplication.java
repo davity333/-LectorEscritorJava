698807d2d036eb546db0e039790fd279916db8fd
david guzman <reyguz421@gmail.com>
2025-12-08 00:04:34 -0600

    agregando logo para la aplicacion

--- src/main/java/org/example/lectorescritor/HelloApplication.java
+++ src/main/java/org/example/lectorescritor/HelloApplication.java
@@ -3,6 +3,7 @@ package org.example.lectorescritor;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,8 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1040, 700);
         stage.setTitle("Escritor-Lectores!");
+        Image icon = new Image(getClass().getResourceAsStream("/images/logo.png"));
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
